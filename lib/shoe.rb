df75e876ec26cea864f76ae0c6ca8a09025f7965
Cedric David <cedric.david@btopenworld.com>
2017-02-14 08:36:13 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,13 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS =[]
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if !BRANDS.include? brand
+      BRANDS << brand
+    end
   end
 
   def cobble
@@ -14,10 +16,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+
 
 
 end
