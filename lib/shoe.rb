215c9f284ebc39d2bb5a02bb72efd5e254b43955
Cedric David <cedric.david@btopenworld.com>
2018-09-03 05:13:33 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,17 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  
 
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
   end
+ def  brand=(brand)
+   @brand
+   BRANDS << brand
+
+ end
 
 
 
