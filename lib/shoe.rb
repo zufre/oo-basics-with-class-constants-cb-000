880fbdb4c3a02d4790c67ff3718a0c254a0a2df2
Cedric David <cedric.david@btopenworld.com>
2018-09-03 05:20:57 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !BRANDS.include? brand
+    if !BRANDS.include?(brand)
       BRANDS << brand
     end
   end
@@ -14,9 +14,8 @@ class Shoe
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+
   end
-
-
 
 
 end
